7e87da03f73d75d1a37aeeaa62c33f961781b63c
AdaGamboaChanona <193223@ids.upchiapas.edu.mx>
2020-11-09 22:15:06 -0600

    config settings para deploy

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
      'USER':config("USER"),
      'PASSWORD': config("PASSWORD"),
      'HOST': config("HOST"),
-     'PORT': config("PORT")  
+     'PORT': config("PORT"),  
  }
 }
 
