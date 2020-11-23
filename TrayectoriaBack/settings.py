a4a3670fc6cd77fb93e2ec766e72914abafffa68
AdaGamboaChanona <193223@ids.upchiapas.edu.mx>
2020-11-23 10:25:28 -0600

    change config settings for deploy server aws

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG",default=False, cast=bool) 
 
-ALLOWED_HOSTS = [config("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [config("ALLOWED_HOSTS"),'54.211.147.44']
 
 
 # Application definition
